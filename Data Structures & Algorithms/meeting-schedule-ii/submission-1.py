6614aa90540d9a95fd8cfcb8cf57ec94b392c0c8
George-ZhiQiao <georgeqiao@microsoft.com>
2026-04-28 21:56:27 -0700

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.py
@@ -0,0 +1,21 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        timeline = []
+        for i in intervals:
+            timeline.append((i.start, 1))
+            timeline.append((i.end, -1))
+        timeline.sort(key=lambda x: (x[0], x[1]))
+
+        res = count = 0
+        for t in timeline:
+            count += t[1]
+            res = max(res, count)
+        return res
